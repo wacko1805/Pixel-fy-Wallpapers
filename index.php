79bf5bee151f544a6336ea6f5f33a6bc897387fd
Jack Sam <54708993+wacko1805@users.noreply.github.com>
2020-11-07 19:42:10 +1100

    Uploaded

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Pixel Wallpapers</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://pixel-fy.com/style.css">
+    </head>
+
+    <body>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="pixel-5/">Pixel 5</a>
+            <a href="pixel-4a/">Pixel 4a</a>
+            <a href="pixel-4/">Pixel 4</a>
+            <a href="shot-on-pixel/">Shot on Pixel</a>
+            <a href="/arts-and-culture/">Arts and culture</a>
+            <a href="https://pixel-fy.com/">Pixel-fy Home</a>
+          </div>
+        <div class="nav">
+            <a onclick="openNav()">Menu</a>
+        </div>
+        <header>
+            <h1>Pixel-fy Wallpapers</h1>
+            <p>Google Pixel Wallpapers</p>
+        </header>
+        <div class="main">
+
+            <h2>About:</h2>
+            <p>Download Google Pixel exclusive wallpapers</p>
+
+        </div>
+        <script src="https://pixel-fy.com/nav.js"></script>
+
+    </body>
+</html>
